d6fa0122bba931c3c0cb04107994b3dd1b469b42
edrian-liao <edrianpaul.liao@duke.edu>
2023-12-09 14:28:10 -0500

    revamped the website

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Store the words you want to cycle through in an array
-const words = ["Kumusta? 👋🏽 I am Ed. ", "How are you? 👋🏽 I am Ed. "];
-let currentWordIndex = 0;
+const words1 = ["Kumusta?👋🏽 Ako si  Ed. ", "How are you?👋🏽 I am Ed. "];
+const words2 = ["Projeykto", "Projects"];
+const words3 = ["Dugtong", "Links"];
+let currentWordIndex = 1;
 
-// Function to update the word every 2 seconds
+// Function to update the word every 5 seconds
 function updateWord() {
-    const changingWord = document.getElementById("changing-word");
-    changingWord.textContent = words[currentWordIndex];
-    currentWordIndex = (currentWordIndex + 1) % words.length;
+    const changingWord1 = document.getElementById("changing-word1");
+    changingWord1.textContent = words1[currentWordIndex];
+    const changingWord2 = document.getElementById("changing-word2");
+    changingWord2.textContent = words2[currentWordIndex];
+    const changingWord3 = document.getElementById("changing-word3");
+    changingWord3.textContent = words3[currentWordIndex];
+    currentWordIndex = (currentWordIndex + 1) % words1.length;
 }
 
 // Update the word initially and set an interval to update it every 2 seconds
 updateWord();
-setInterval(updateWord, 3000); 
+setInterval(updateWord, 5000); 
